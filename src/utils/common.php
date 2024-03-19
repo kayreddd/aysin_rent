09b1cb1a078f75cdafaf123abad18fa8d6778445
iblms <ilyas170605@gmail.com>
2024-03-19 11:34:44 +0100

    création de l'arborescence

---
+++ src/utils/common.php
@@ -0,0 +1,7 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+define('PROJECT_FOLDER', '/aysin_rent/');
+define('SITE_ROOT', $_SERVER['DOCUMENT_ROOT'] . PROJECT_FOLDER);
