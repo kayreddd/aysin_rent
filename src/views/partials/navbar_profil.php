25ebcbfb7ed10f82e575fb2edb3104ca2f2b3a46
iblms <ilyas170605@gmail.com>
2024-04-02 13:30:18 +0200

    correction du css après conflit

--- src/views/partials/navbar_profil.php
+++ src/views/partials/navbar_profil.php
@@ -4,14 +4,13 @@
 <body>
     <main>
         <div class="all-btn">
-                <a href="<?=PROJECT_FOLDER ?>www/?p=historical" class="btn-menu" onclick="changeColor(this)">MES LOCATIONS</a>
-                <a href="<?=PROJECT_FOLDER ?>www/?p=myInformations" class="btn-menu" onclick="changeColor(this)">MES INFORMATIONS</a>
-                <a href="<?=PROJECT_FOLDER ?>www/?p=changeMdp" class="btn-menu" onclick="changeColor(this)">CHANGER MON MOT DE PASSE</a>
-                <a href="<?=PROJECT_FOLDER ?>www/?p=" class="btn-menu" onclick="changeColor(this)">MODE DE PAIEMENT</a>
-                <a href="<?=PROJECT_FOLDER ?>www/?p=" class="btn-menu" onclick="changeColor(this)">DÉCONNEXION</a>
-            </div>
-        </main>
-        <script src="<?=PROJECT_FOLDER ?>www/js/navbar_profil.js"></script>
+            <a href="<?=PROJECT_FOLDER ?>www/?p=historical" class="btn-menu" onclick="changerFond(this)">MES LOCATIONS</a>
+            <a href="<?=PROJECT_FOLDER ?>www/?p=myInformations" class="btn-menu" onclick="changerFond(this)">MES INFORMATIONS</a>
+            <a href="<?=PROJECT_FOLDER ?>www/?p=changeMdp" class="btn-menu" onclick="changerFond(this)">CHANGER MON MOT DE PASSE</a>
+            <a href="<?=PROJECT_FOLDER ?>www/?p=" class="btn-menu" onclick="changerFond(this)">MODE DE PAIEMENT</a>
+            <a href="<?=PROJECT_FOLDER ?>www/?p=" class="btn-menu" onclick="changerFond(this)">DÉCONNEXION</a>
+        </div>
+        <!-- <script src="<?=PROJECT_FOLDER ?>www/js/navbar_profil.js"></script> -->
     </main>
 </body>
 
